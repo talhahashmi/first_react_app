de0c86f84065cdb1a0b162f150d9d111cefa18ac
talhahashmi <talha.hashmi01@gmail.com>
2024-02-05 16:38:17 +0500

    update in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Hello World from Talha
+      Hello World from Talha !
     </div>
   );
 }
